098007bc973b5fb3042722f37b6b730f5f579a61
szkabaroli <szkabaroli@gmail.com>
2017-04-17 12:47:11 +0200

    nodemon and index.html

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname , 'index.html'));
 });
 
 app.listen(8080, () => console.log('We are running on localhost:8080'));
